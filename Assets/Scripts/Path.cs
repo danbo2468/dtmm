d90ed09ef401eb3035ab497c316617cee102920a
Marcel  Oostebring <moostebring@hotmail.com>
2016-12-02 16:47:50 +0100

    Added possibility to get core world nodes and reachable core world nodes
    public List<Transform> GetAllCoreNodes()
    public List<Transform> GetReachableCoreNodes(List<Transform> coreNodes)
    
    ## No comments yet ##

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -5,34 +5,70 @@ public class Path : MonoBehaviour {
     // https://www.youtube.com/watch?v=Lo5PPqHmsIM
     public Color lineColor;
 
-    private List<Transform> nodes;
+    private List<Transform> drawNodes;
+    private List<Transform> coreNodes;
+    private List<Transform> reachableNodes;
+    private List<Transform> pathNodes;
 
+    // This is just for unity, drawing the line on the worldmap.
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = lineColor;
-        nodes = new List<Transform>();
+        drawNodes = new List<Transform>();
         Transform[] pathTransforms = GetComponentsInChildren<Transform>();
 
         for (int i = 0; i < pathTransforms.Length; i++)
             if (pathTransforms[i] != transform)
-                nodes.Add(pathTransforms[i]);
+                drawNodes.Add(pathTransforms[i]);
 
-        for (int i = 0; i < nodes.Count; i++)
+        for (int i = 0; i < drawNodes.Count; i++)
         {
-            Vector3 currentNode = nodes[i].position;
+            Vector3 currentNode = drawNodes[i].position;
             Vector3 previousNode;
 
             if (i > 0)
-                previousNode = nodes[i - 1].position;
+                previousNode = drawNodes[i - 1].position;
             else
                 previousNode = currentNode;
 
             Gizmos.DrawLine(previousNode, currentNode);
             Gizmos.DrawWireSphere(currentNode, 0.2f);
         }
-
-        
-
     }
+
+    public List<Transform> GetAllCoreNodes()
+    {
+        coreNodes = new List<Transform>();
+        Transform[] nodeTransforms = GetComponentsInChildren<Transform>();
+        for (int i = 0; i < nodeTransforms.Length; i++)
+            if (nodeTransforms[i] != transform && nodeTransforms[i].tag != "Path")
+                coreNodes.Add(nodeTransforms[i]);
+
+        Debug.Log("Expected number = 6, actual number: " + coreNodes.Count);
+        return coreNodes;
+    }
+
+    public List<Transform> GetReachableCoreNodes(List<Transform> coreNodes)
+    {
+        reachableNodes = new List<Transform>();
+        for (int i = 0; i < coreNodes.Count; i++)
+            if (coreNodes[i].tag != "LevelUnreachable")
+                reachableNodes.Add(coreNodes[i]);
+
+        Debug.Log("Expected number = 3, actual number: " + reachableNodes.Count);
+        return reachableNodes;
+    }
+
+    public List<Transform> GetNextUnreachableNode()
+    {
+        return coreNodes;
+    }
+
+    public List<Transform> GetPathNodesFromCoreNode(Transform node)
+    {
+        pathNodes = new List<Transform>();
+        return pathNodes;
+    }
+
 
 }
--- Assets/Scripts/WorldController.cs
+++ Assets/Scripts/WorldController.cs
@@ -4,20 +4,30 @@ using System.Collections;
 public class WorldController : MonoBehaviour {
     [SerializeField]
     public GameObject player;
+    [SerializeField]
+    public GameObject path;
+
+    public Path script;
 
 	// Use this for initialization
 	void Start () {
-	    // Ask GameManager for all level completions and highscores.
-	}
+        // Ask GameManager for all level completions and highscores.
+        script = path.GetComponent<Path>();
+        HandleMovement();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        HandleMovement();
+
+        
 	}
 
     public void HandleMovement()
     {
-
+        //player.transform.position = Vector2.Lerp()
+        script.GetReachableCoreNodes(script.GetAllCoreNodes());
     }
+
     
+   
 }
