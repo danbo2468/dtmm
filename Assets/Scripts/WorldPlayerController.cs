f53ccd9058bfee0ff6716c6fe29bd60c81a55516
Daniël Boonstra <daniel.boonstra@outlook.com>
2016-12-21 15:31:45 +0100

    Started working on the overworld animation for the character

---
+++ Assets/Scripts/WorldPlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldPlayerController : MonoBehaviour {
+
+    private Vector3 previousLocation;
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+    void Update()
+    {
+        Vector3 curVel = (transform.position - previousLocation) / Time.deltaTime;
+        animator.SetFloat("y", curVel.y);
+        previousLocation = transform.position;
+    }
+}
