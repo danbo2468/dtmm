3a645fcfd2d9aaffa91e14804dcec374dab6ac5f
Daniël Boonstra <daniel.boonstra@outlook.com>
2017-01-18 15:18:29 +0100

    Added sound to the main menu

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+
+    AudioSource soundtrack;
+
+	// Use this for initialization
+	void Start () {
+        soundtrack = GetComponent<AudioSource>();
+        Debug.Log(GameManager.gameManager.backgroundMusic);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameManager.gameManager.backgroundMusic)
+        {
+            soundtrack.volume = 1;
+            Debug.Log("FUCKING SONG SHOULD PLAY");
+        } else {
+            soundtrack.volume = 0;
+        }
+    }
+}
