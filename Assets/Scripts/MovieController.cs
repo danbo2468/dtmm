9e1b2666070d9af50e13abec6c14f552a24329e3
Daniël Boonstra <daniel.boonstra@outlook.com>
2017-01-19 14:15:00 +0100

    Start for video controller

---
+++ Assets/Scripts/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovieController : MonoBehaviour {
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        Handheld.PlayFullScreenMovie("Dummy 1.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
