4e3b26f3844843e8c251e87dde837c350a45ddef
Daniel Boonstra <daniel.boonstra@outlook.com>
2017-01-30 19:59:50 +0100

    Handheld movies partial fix?

--- Assets/Scripts/MovieController.cs
+++ Assets/Scripts/MovieController.cs
@@ -8,6 +8,7 @@ public class MovieController : MonoBehaviour {
 
     public MovieTexture movieMale;
     public MovieTexture movieFemale;
+    public int movieNumber;
     private MovieTexture movie;
     private AudioSource audio;
 
@@ -21,30 +22,51 @@ public class MovieController : MonoBehaviour {
             movie = movieFemale;
         }
 
-        GetComponent<RawImage>().texture = movie;
-        audio = GetComponent<AudioSource>();
-        audio.clip = movie.audioClip;
-        movie.Play();
-        audio.Play();
+        #if UNITY_ANDROID
+            string file;
+            if (GameManager.gameManager.characterGender == "Male")
+            {
+                file = "Boy" + movieNumber + ".mp4";
+            } else {
+                file = "Girl" + movieNumber + ".mp4";
+            }
+            Handheld.PlayFullScreenMovie(file);
+        #endif
+
+        #if UNITY_EDITOR
+            GetComponent<RawImage>().texture = movie;
+            audio = GetComponent<AudioSource>();
+            audio.clip = movie.audioClip;
+            movie.Play();
+            audio.Play();
+        #endif
     }
 
     // Update is called once per frame
     void Update () {
-        if (!movie.isPlaying)
-        {
-            if (GameManager.gameManager.levelManager.isFinished)
+
+        #if UNITY_ANDROID
+        
+        #endif
+
+        #if UNITY_EDITOR
+            if (!movie.isPlaying)
             {
-                GameManager.gameManager.levelManager.finishedMenu.SetActive(true);
-                GameManager.gameManager.levelManager.makeHeartsShrink = true;
-                gameObject.SetActive(false);
+                if (GameManager.gameManager.levelManager.isFinished)
+                {
+                    GameManager.gameManager.levelManager.finishedMenu.SetActive(true);
+                    GameManager.gameManager.levelManager.makeHeartsShrink = true;
+                    gameObject.SetActive(false);
+                }
+                else
+                {
+                    gameObject.SetActive(false);
+                    GameManager.gameManager.levelManager.Resume();
+                }
             }
-            else
-            {
-                gameObject.SetActive(false);
-                GameManager.gameManager.levelManager.Resume();
-            }
-        }
-	}
+        #endif
+
+    }
 
     // Play the file
     public void Play()
